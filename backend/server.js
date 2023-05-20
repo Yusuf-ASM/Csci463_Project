8da1597a701000c3fefa20e6b11d392ac0523162
y.alsayed <y.alsayed@nu.edu.eg>
2023-05-20 08:59:25 +0300

    with cooldown

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,48 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+let egyptianIp = false;
+let coolDown = 0;
+let lastRequest = Date.now();
 
-app.use('*',(req,res,next)=>{
-    console.log(req.ip);
-    next();
+app.get("/egip", (req, res) => {
+  egyptianIp = !egyptianIp;
+  res.sendStatus(200);
 });
-app.all("*", (req, res) => {
+
+app.get("/cooldown", (req, res) => {
+  if (coolDown == 0) {
+    coolDown = 5 * 1000;
+  } else {
+    coolDown = 0;
+  }
+  res.sendStatus(200);
+});
+
+app.use("*", (req, res, next) => {
+  console.log(req.ip);
+  console.log(egyptianIp);
+  console.log(coolDown);
+  console.log(lastRequest);
+  if (coolDown != 0 && Date.now() - lastRequest < coolDown) {
+  } else {
+    lastRequest = Date.now();
+    if (egyptianIp) {
+      if (detector.isEgyptianIp(detector.parseIp(req.ip))) {
+        next();
+      } else {
+        res.send("Not egyptian ip :(");
+      }
+    } else {
+      next();
+    }
+  }
+});
+
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "home.html"));
 });
 
-app.listen(8080,()=>{
-    console.log("running :)");
+app.listen(8080, () => {
+  console.log("running :)");
 });
